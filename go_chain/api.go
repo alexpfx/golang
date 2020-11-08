3087bee504956c546eecc8ce1f924f35b9c5e7c9
alexpfx <alexpfba@gmail.com>
2020-11-07 23:48:51 -0300

    go chain

--- go_chain/api.go
+++ go_chain/api.go
@@ -1,6 +1,65 @@
 package go_chain
 
+import (
+	js "encoding/json"
+	"github.com/pelletier/go-toml"
+)
 
-func Parse (filePath string) (Request, error){
-	return Request{}, nil
+func Parse(filePath string) (*Request, error) {
+	tree, err := toml.LoadFile(filePath)
+
+	if err != nil {
+		return nil, err
+	}
+	var x Request
+
+	err = tree.Unmarshal(&x)
+	check(err)
+
+	//unquoted, err := strconv.Unquote(x.Json)
+
+	message := js.RawMessage(x.Json)
+
+	unquoted, _ := message.MarshalJSON()
+
+	request, err2 := check(err)
+	if err2 != nil {
+		return request, err2
+	}
+	return &Request{
+		Method:   x.Method,
+		Output:   x.Output,
+		Input:    x.Input,
+		Endpoint: x.Endpoint,
+		Json:     string(unquoted),
+	}, nil
+}
+
+func check(err error) (*Request, error) {
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func ReplaceInput(json string, input map[string]string) (string, error) {
+	var value interface{}
+	json.replace(/\\n/g, '')
+	err := js.Unmarshal([]byte(json), value)
+
+	if err != nil {
+		return "", err
+	}
+
+	var msgMap = value.(map[string]string)
+
+	for key, value := range input {
+		msgMap[key] = value
+	}
+
+	marshal, err := js.Marshal(value)
+	if err != nil {
+		return "", err
+	}
+	return string(marshal), nil
 }
--- go_chain/go_chain_test/api_test.go
+++ go_chain/go_chain_test/api_test.go
@@ -3,20 +3,106 @@ package go_chain_test
 import (
 	"fmt"
 	"github.com/alexpfx/golang/go_chain"
-	"github.com/pelletier/go-toml"
 	"testing"
 )
 
-func TestParse(t *testing.T) {
-	filePath := "data/judicial.toml"
-	tree, err := toml.LoadFile(filePath)
-	var x go_chain.Request
+const fragment = "%s: esperava %v, porém obteve %v"
+const errFragment = "espera que erro fosse %v, porém obteve %v"
 
-	tree.Unmarshal(&x)
+func TestReplaceInput(t *testing.T) {
+	tests := []struct {
+		name  string
+		input map[string]string
+	}{
+		{"", map[string]string{"cpf": "1122"}},
+	}
+	filePath := fmt.Sprintf("data/%s", "inclusao1.toml")
+
+	parse, err := go_chain.Parse(filePath)
 	if err != nil {
-		fmt.Println(err)
+		t.Error(err)
 	}
 
-	go_chain.Parse(filePath)
+	json := parse.Json
 
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			input, err := go_chain.ReplaceInput(json, map[string]string{"cpf": "1122"})
+			checkErr(t, err)
+
+			t.Log(input)
+
+		})
+	}
+}
+
+func checkErr(t *testing.T, err error) {
+	if err != nil {
+		t.Errorf("erro inexperado: %v", err)
+	}
+}
+func TestParsing(t *testing.T) {
+	tests := []struct {
+		name     string
+		file     string
+		expected go_chain.Request
+		e        error
+	}{
+		{"judicial", "judicial.toml", go_chain.Request{
+			Method:   "post",
+			Endpoint: "{{ baseUrl }}/PortalSibe",
+			Input:    []string{"cpf", "nome"},
+			Output:   []string{"nb"},
+		}, nil},
+		{"massa", "massa.toml", go_chain.Request{
+			Method: "get",
+		}, nil},
+		{"inclusao1", "inclusao1.toml", go_chain.Request{
+			Method:   "post",
+			Endpoint: "{{ baseUrl }}/sibews/rest/requerimento/incluir/bi/inicial",
+			Input:    []string{"cpf", "nomeTitular"},
+			Output:   []string{"nb"},
+		}, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			filePath := fmt.Sprintf("data/%s", test.file)
+
+			r, e := go_chain.Parse(filePath)
+
+			t.Log(test.name)
+
+			if test.e != e {
+
+				t.Errorf(errFragment, test.e, e)
+				return
+			}
+
+			expected := test.expected
+
+			if r.Endpoint != expected.Endpoint {
+				t.Errorf(fragment, "endpoint", expected.Endpoint, r.Endpoint)
+			}
+
+			if r.Method != expected.Method {
+				t.Errorf(fragment, "method", expected.Method, r.Method)
+			}
+
+			for i, input := range r.Input {
+				expectedInput := expected.Input[i]
+				if input != expectedInput {
+					t.Errorf(fragment, "input", expectedInput, input)
+				}
+			}
+
+			for i, output := range r.Output {
+				expectedOutput := expected.Output[i]
+				if output != expectedOutput {
+					t.Errorf(fragment, "output", expectedOutput, output)
+				}
+			}
+
+		})
+	}
 }
